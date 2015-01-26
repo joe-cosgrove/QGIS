547b86760028a46159436b8465d8c47e3379234f
Nyall Dawson <nyall.dawson@gmail.com>
2015-01-27 09:25:20 +1100

    Enable coverage reports for Travis builds
    Changes required:
    - switch Travis builds to gcc, using O0 for build speed
    - enable a debug build
    - enable coverage report for ctest on Travis (sent to dash)

--- ci/travis/linux/install.sh
+++ ci/travis/linux/install.sh
@@ -13,6 +13,7 @@ cmake -DWITH_SERVER=ON \
       -DWITH_APIDOC=ON \
       -DWITH_ASTYLE=ON \
       -DWITH_PYSPATIALITE=ON \
+      -DENABLE_COVERAGE=ON \
       -DGRASS_PREFIX7=/usr/lib/grass70 \
       -DGRASS_INCLUDE_DIR7=/usr/lib/grass70/include \
       -DCXX_EXTRA_FLAGS="$CLANG_WARNINGS" \
