86fa4b421519fe4a47fd2f1745cff9bb39478ffa
Nyall Dawson <nyall.dawson@gmail.com>
2015-08-14 08:53:16 +1000

    Attempt at coveralls

--- ci/travis/linux/before_script.sh
+++ ci/travis/linux/before_script.sh
@@ -2,4 +2,6 @@ printf "[qgis_test]\nhost=localhost\ndbname=qgis_test\nuser=postgres" > ~/.pg_se
 psql -c 'CREATE DATABASE qgis_test;' -U postgres
 psql -c 'CREATE EXTENSION postgis;' -U postgres -d qgis_test
 psql -f $TRAVIS_BUILD_DIR/tests/testdata/provider/testdata.sql -U postgres -d qgis_test
+lcov --directory . --zerocounters
+
 
