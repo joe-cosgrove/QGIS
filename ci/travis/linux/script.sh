9fb872d8cdb146ad18aa7e80dee16317056fe044
Nyall Dawson <nyall.dawson@gmail.com>
2015-08-25 08:22:57 +1000

    Report memory leaks from lsan

--- ci/travis/linux/script.sh
+++ ci/travis/linux/script.sh
@@ -1,2 +1,4 @@
+export ASAN_OPTIONS=detect_leaks=1
+#No Python tests due to ASAN incompatibility
 xvfb-run ctest -V -E 'qgis_openstreetmaptest|qgis_wcsprovidertest|Py' -S ./qgis-test-travis.ctest --output-on-failure
 
