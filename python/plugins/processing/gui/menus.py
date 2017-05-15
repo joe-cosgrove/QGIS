6dc964d4fefeac825812eb6970fba9c89c2357dc
Nyall Dawson <nyall.dawson@gmail.com>
2017-05-15 12:46:59 +1000

    Port getCustomParametersDialog to QgsProcessingAlgorithm

--- python/plugins/processing/gui/menus.py
+++ python/plugins/processing/gui/menus.py
@@ -205,7 +205,7 @@ def _executeAlgorithm(alg):
 
     context = dataobjects.createContext()
     if (alg.countVisibleParameters() + alg.countVisibleOutputs()) > 0:
-        dlg = alg.getCustomParametersDialog()
+        dlg = alg.createCustomParametersWidget(None)
         if not dlg:
             dlg = AlgorithmDialog(alg)
         canvas = iface.mapCanvas()
