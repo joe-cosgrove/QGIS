76a3a68ca231b6e5fea003505fabd52b4ee62f63
Nyall Dawson <nyall.dawson@gmail.com>
2017-05-17 07:50:01 +1000

    Move expression context generation out of parameters

--- python/plugins/processing/core/GeoAlgorithm.py
+++ python/plugins/processing/core/GeoAlgorithm.py
@@ -124,7 +124,6 @@ class GeoAlgorithm(QgsProcessingAlgorithm):
         try:
             self.setOutputCRS()
             self.resolveOutputs()
-            self.evaluateParameterValues()
             self.runPreExecutionScript(feedback)
             self.processAlgorithm(parameters, context, feedback)
             feedback.setProgress(100)
@@ -232,14 +231,6 @@ class GeoAlgorithm(QgsProcessingAlgorithm):
             if ext in exts:
                 return name
         return 'GTiff'
-
-    def evaluateParameterValues(self):
-        for param in self.parameters:
-            try:
-                param.evaluate(self)
-            except ValueError as e:
-                traceback.print_exc()
-                raise GeoAlgorithmExecutionException(str(e))
 
     def resolveOutputs(self):
         """Sets temporary outputs (output.value = None) with a
